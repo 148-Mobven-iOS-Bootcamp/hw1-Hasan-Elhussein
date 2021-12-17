9e88e9ca62a430ae5df512ace5df0b4f7b0852aa
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-12-18 00:26:56 +0300

    Add comments to explain the code

--- FibonacciSeries.playground/Contents.swift
+++ FibonacciSeries.playground/Contents.swift
@@ -3,13 +3,16 @@ import UIKit
 var fibArr = [Int]()
 
 func calculateFibonacci(_ x1: Int = 0, _ x2: Int = 1, lowerLimit: Int, upperLimit: Int) {
-    let sum = x1 + x2
+    let sum = x1 + x2 // Calculate fibonacci number
     
+    // If the number is bigger than the upper limit, stop the function.
     if sum > upperLimit { return }
+    // If the number is smaller than the lower limit, add it to fibArr array.
     if sum > lowerLimit { fibArr.append(sum) }
-    
+    // Recursion.
     calculateFibonacci(x2, sum, lowerLimit: lowerLimit, upperLimit: upperLimit)
 }
 
+// Run the function and print out the output.
 calculateFibonacci(lowerLimit: 750, upperLimit: 1000)
 print(fibArr)
